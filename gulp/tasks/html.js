84cb8cac75629624f57952cdcec62989b98a473a
Valentin <44469298+Infinity-Kim@users.noreply.github.com>
2020-10-15 16:51:46 +0300

    Update

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,17 +2,10 @@ module.exports = () => {
     $.gulp.task('html', () => {
         return $.gulp.src('./src/pages/*.pug')
             .pipe($.glp.plumber({errorHandler: $.glp.notify.onError("Error: <%= error.message %>")}))
-            .pipe($.glp.pug())
-            .pipe($.glp.htmlBeautify({
-                indentSize: 2,
-                unformatted: [
-                    'abbr', 'area', 'b', 'bdi', 'bdo', 'br', 'cite',
-                    'code', 'data', 'datalist', 'del', 'dfn', 'em', 'embed', 'i', 'ins', 'kbd', 'keygen', 'map', 'mark', 'math', 'meter', 'noscript',
-                    'object', 'output', 'progress', 'q', 'ruby', 's', 'samp', 'small',
-                    'strong', 'sub', 'sup', 'template', 'time', 'u', 'var', 'wbr', 'text',
-                    'acronym', 'address', 'big', 'dt', 'ins', 'strike', 'tt'
-                ]
+            .pipe($.glp.pug({
+                pretty: true
             }))
+            .pipe($.glp.htmlPrettify({indent_char: ' ', indent_size: 2}))
             .pipe($.glp.w3cHtmlValidator())
             .pipe($.gulp.dest('./build'))
             .pipe($.browserSync.reload({ stream: true }))
--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,11 +3,11 @@ module.exports = () => {
         $.browserSync.init({
             server: './build'
         });
-
+        $.gulp.series('fonts', 'img', 'scss', 'html');
         $.gulp.watch('./src/fonts/**/*',  $.gulp.series('fonts')); // шрифты
         $.gulp.watch('./src/img/**/*.{png,jpg,jpeg}', $.gulp.series('img')); // картинки
 
-        $.gulp.watch('./src/scss/*.scss', $.gulp.series('scss')); // стили
-        $.gulp.watch('./src/pages/*.pug', $.gulp.series('html')); // html
+        $.gulp.watch(['./src/scss/*.scss', './src/scss/**/*.scss'], $.gulp.series('scss')); // стили
+        $.gulp.watch(['./src/pages/*.pug', './src/pages/**/*.pug'], $.gulp.series('html')); // html
     });
 };
